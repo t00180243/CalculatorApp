543343d86176d5fc8c89373d26556ed35869e857
Aoife Horgan <t00180243@gmail.com>
2018-04-12 23:28:08 +0100

    Updating UI tests

--- app/src/androidTest/java/devops/aoifehorgan/me/calculatormobileapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/devops/aoifehorgan/me/calculatormobileapp/ExampleInstrumentedTest.java
@@ -2,11 +2,18 @@ package devops.aoifehorgan.me.calculatormobileapp;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +23,26 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
 
-        assertEquals("devops.aoifehorgan.me.calculatormobileapp", appContext.getPackageName());
+    @Test
+    public void testAddition() {
+        // Type text and then press the button.
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.buttonAdd)).perform(click());
+        onView(withId(R.id.button2)).perform(click());
+        onView(withId(R.id.textViewAns)).check(matches(withText("2+2")));
+        onView(withId(R.id.buttonEql)).perform(click());
+        onView(withId(R.id.textViewAns)).check(matches(withText("1.0")));
+
+    }
+    @Test
+    public void clear() {
+        onView(withId(R.id.buttonDel)).perform(click());
+        onView(withId(R.id.textViewAns)).check(matches(withText("")));
     }
 }
+
+
